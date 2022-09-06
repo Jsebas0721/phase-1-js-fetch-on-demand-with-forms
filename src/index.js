238a5f86d201fef1a7b1f870e5f9081dfa069922
sebastian medina <sebastianlopez_21@hotmail.com>
2022-09-06 10:58:09 -0400

    commit tree e0ab7671685fbebd2a482c98a549265d129fd7a2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
   
-}
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) =>{
+        event.preventDefault();
+        console.log(event);
+    const input = event.target.searchByID.value;
+        console.log(input);
+
+
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innetText = data.title;
+            summary.innerText = data.summary;
+        });
+    });   
+}   
 
 document.addEventListener('DOMContentLoaded', init);
